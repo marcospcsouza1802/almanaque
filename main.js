e245d84b88a797db031ca2d102e4cadde775b1a0
Marcos <marcospcsouza1802@gmail.com>
2022-11-07 11:10:38 -0300

    delay botoes

---
+++ main.js
@@ -0,0 +1,8 @@
+function showSubmit() {
+    document.getElementById('precoOriginal').style.display = 'block';
+    document.getElementById('ofertaLimitada').style.display = 'block';
+    document.getElementById('btnComprar').style.display = 'block';
+    document.getElementById('precoPromocional').style.display = 'block';
+}
+
+setTimeout(showSubmit, 180000);
