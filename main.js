1e05dae349b8b16cc88bc7c2b9c72910645a53ac
Marcos <marcospcsouza1802@gmail.com>
2022-11-14 15:40:50 -0300

    correção delay botao

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 function showSubmit() {
-    document.getElementById('precoOriginal').style.display = 'block';
-    document.getElementById('ofertaLimitada').style.display = 'block';
     document.getElementById('btnComprar').style.display = 'block';
-    document.getElementById('precoPromocional').style.display = 'block';
+
 }
 
-setTimeout(showSubmit, 0);
+setTimeout(showSubmit, 180000);
