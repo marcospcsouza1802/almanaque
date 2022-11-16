eeba6ee317b69fc47c984b9cf65ac9c38e842823
Marcos <marcospcsouza1802@gmail.com>
2022-11-16 09:43:45 -0300

    voltar preço

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 function showSubmit() {
     document.getElementById('btnComprar').style.display = 'block';
+    document.getElementById('precoPromocional').style.display = 'block';
+    document.getElementById('ofertaLimitada').style.display = 'block';
+    document.getElementById('precoOriginal').style.display = 'block';
+
 
 }
 
